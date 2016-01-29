9328ee8c758287ef293d0f627856e36b66d61939
Charles Carter <charles.h.carter@gmail.com>
2016-01-29 12:19:51 -0800

    pass specs for both pizza object and total cost calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
-// Business Logic
 function Pizza(girth, toppings) {
   this.girth = girth;
-  this.toppings = [];
-  this.totalCost = 0;
+  this.toppings = toppings;
+  this.totalCost;
 }
 
+Pizza.prototype.totalCost = function() {
+  var base = 0;
+  var cost = 0;
+  var toppingCost = 0;
 
-
-// cost calculator prototype method
-// for each topping push to toppings []
-Pizza.prototype.toppings(allToppings) {
-}
-
-Pizza.prototype.totalCost() {
-  if (this.girth = "small") {
+  if (this.girth === "small") {
     base = 10;
-    toppingCost = (this.toppings.length * ".5");
-    this.totalCost = base + toppingCost;
-  } else if (this.girth = "medium") {
+  } else if (this.girth === "medium") {
     base = 15;
-    toppingCost = (this.toppings.length * ".5");
-    this.totalCost = base + toppingCost;
-  } else if (this.girth = "large") {
+  } else if (this.girth === "large") {
     base = 20;
-    toppingCost = (this.toppings.length * ".5");
-    this.totalCost = base + toppingCost;
-  } else {
-    this.totalCost = this.totalCost;
-  };
+  }
+  toppingCost = (this.toppings * .5 );
+  cost = base + toppingCost;
+  this.totalCost = cost;
+  return cost;
 };
+//
+// END OF BUSINESS LOGIC
+//
+// gather input from data fields
+//
+// create new pizza object
+//
+// do the math! and return the cost!
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,11 @@
-describe("pizza", function() {
-  it("instantiates a pizza object with the given properties", function() {
-    var testPizza = new Pizza("small", "[cheese]");
+describe('Pizza', function() {
+  it("creates a pizza object with the given properties", function() {
+    var testPizza = new Pizza ("small", 3);
     expect(testPizza.girth).to.equal("small");
-    expect(testPizza.toppings).to.eql([]);
-    expect(testPizza.totalCost).to.equal(0);
+    expect(testPizza.toppings).to.equal(3);
   });
-  it("will calculate the total cost of the pie", function
+  it("will calculate the total cost of the pie", function() {
+    var testPizza = new Pizza ("small",4);
+    expect(testPizza.totalCost()).to.equal(12.00);
+  });
 });
-//
