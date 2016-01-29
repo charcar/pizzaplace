f2dc3d07c7f6b65dadef4ff6a87b52ff6af6fbb3
Charles Carter <charles.h.carter@gmail.com>
2016-01-29 10:51:31 -0800

    create pizza object and calculate cost, almost passing second cost spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 // Business Logic
 function Pizza(girth, toppings) {
   this.girth = girth;
-  this.toppings = toppings;
+  this.toppings = [];
   this.totalCost = 0;
 }
+
+
+
+// cost calculator prototype method
+// for each topping push to toppings []
+Pizza.prototype.toppings(allToppings) {
+}
+
+Pizza.prototype.totalCost() {
+  if (this.girth = "small") {
+    base = 10;
+    toppingCost = (this.toppings.length * ".5");
+    this.totalCost = base + toppingCost;
+  } else if (this.girth = "medium") {
+    base = 15;
+    toppingCost = (this.toppings.length * ".5");
+    this.totalCost = base + toppingCost;
+  } else if (this.girth = "large") {
+    base = 20;
+    toppingCost = (this.toppings.length * ".5");
+    this.totalCost = base + toppingCost;
+  } else {
+    this.totalCost = this.totalCost;
+  };
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,10 @@
 describe("pizza", function() {
   it("instantiates a pizza object with the given properties", function() {
-    var testPizza = new Pizza("small", "cheese");
+    var testPizza = new Pizza("small", "[cheese]");
     expect(testPizza.girth).to.equal("small");
-    expect(testPizza.toppings).to.equal("cheese");
+    expect(testPizza.toppings).to.eql([]);
     expect(testPizza.totalCost).to.equal(0);
-
   });
+  it("will calculate the total cost of the pie", function
 });
+//
